111ed4ba95ce85b9efa9db334cfb614516d5ebda
usmc1371nerd <james.p.dumas@gmail.com>
2023-04-18 21:38:10 -0500

    project files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {AuthProvider} from "react-auth-kit"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthProvider
+    authType='cookie'
+    authName='_auth'
+    cookieDomain={window.location.hostname}
+    cookieSecure
+     />
+  </React.StrictMode>
+);
