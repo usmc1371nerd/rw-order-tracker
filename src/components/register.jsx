e9515516391d12a331f8f6de115c3ddcdf719441
usmc1371nerd <james.p.dumas@gmail.com>
2023-04-24 14:59:25 -0500

    install node sass and started register comp

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,29 @@
- import React from "react"
+ import React, {useState} from "react"
+ 
  
  
  const Register = ()=> {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = (e) => {  
+      e.preventDefault();
+      console.log(email);
+      console.log(password);
+    }
    return (
+    <div>
     <h1>Register</h1>
+
+
+ 
+        <form onSubmit={handleSubmit}>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='email' id='email'></input>
+        <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder='password' id='password'></input>
+        <input value={password} onChange={(e) => setPassword(e.target.value)}type="password" placeholder='password' id='password'></input>
+        <button type='submit' >Login</button>
+        </form>
+   </div>
    )
-    
 }
 
 export default Register
